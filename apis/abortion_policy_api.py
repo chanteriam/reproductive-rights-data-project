f7b212e40225ee1b76bf917d715b3b5b7d464c4f
Chanteria Milner <chanteria.milner@gmail.com>
2023-01-30 16:32:55 -0600

    API data cleaning file

--- apis/abortion_policy_api.py
+++ apis/abortion_policy_api.py
@@ -3,9 +3,15 @@
 import requests
 import os
 import json
+import re
 
 APIKEY = os.environ.get('ABORTION_POLICY_API_KEY')
 HEADERS = {'token': APIKEY}
+TYPE_DEFAULTS = {
+    'str': None,
+    'bool': False,
+    'int': 0,
+    'float': 0.0}
 
 
 def get_states(filepath):
@@ -25,7 +31,7 @@ def get_states(filepath):
     return states
 
 
-def get_state_data():
+def get_api_data():
     """
     Return state policy data for: gestational limits, insurance coverage, minors,
         and waiting periods
@@ -50,50 +56,24 @@ def get_state_data():
     states_minors = r_minors.json()
     states_waiting = r_waiting.json()
 
+    # Dump to json
+    print("Writing out to json file")
+    with open("data/abortion_policy_api_gestational_unclean.json", "w") as f:
+        json.dump(states_gestational, f, indent=1)
+
+    with open("data/abortion_policy_api_insurance_unclean.json", "w") as f:
+        json.dump(states_insurance, f, indent=1)
+
+    with open("data/abortion_policy_api_minors_unclean.json", "w") as f:
+        json.dump(states_minors, f, indent=1)
+
+    with open("data/abortion_policy_api_waiting_unclean.json", "w") as f:
+        json.dump(states_waiting, f, indent=1)
+
     return states_gestational, states_insurance, states_minors, states_waiting
 
 
-def to_json(g, i, m, w):
-    """
-    Dump state policy data to json file(s)
-
-    Inputs:
-        g (list): gestational limit policy by state
-        i (list): insurance coverage policy by state
-        m (list): minors policy by state
-        w (list): waiting periods by state
-    """
-
-    # sort states data
-    g_sort = sorted(g)
-    i_sort = sorted(i)
-    m_sort = sorted(m)
-    w_sort = sorted(w)
-
-    print("Writing out to json file")
-
-    # one large json file
-    with open("data/abortion_policy_api.json", "w") as f:
-        json.dump(g_sort, f, indent=1)
-        json.dump(i_sort, f, indent=1)
-        json.dump(m_sort, f, indent=1)
-        json.dump(w_sort, f, indent=1)
-
-    # json file per policy area
-    with open("data/abortion_policy_api_gestational.json", "w") as f:
-        json.dump(g_sort, f, indent=1)
-
-    with open("data/abortion_policy_api_insurance.json", "w") as f:
-        json.dump(i_sort, f, indent=1)
-
-    with open("data/abortion_policy_api_minors.json", "w") as f:
-        json.dump(m_sort, f, indent=1)
-
-    with open("data/abortion_policy_api_waiting.json", "w") as f:
-        json.dump(w_sort, f, indent=1)
-
-
-def fix_missing_data(g, i, m, w):
+def fix_missing_data(state_policies, states):
     """
     Fill in missing characteristics for each state
 
@@ -103,5 +83,84 @@ def fix_missing_data(g, i, m, w):
         m (list): minors policy by state
         w (list): waiting periods by state
     """
+    pattern = r'[!.,\'"?:<>]'
+    keys_and_defaults = {}
 
-    # TODO
+    # set default types of each state policy
+    for _, state_info in state_policies.items():
+        for k, v in state_info.items():
+            if k not in keys_and_defaults.keys():
+                key = re.sub(pattern, '', str(type(v))).split()[-1]
+                keys_and_defaults[k] = TYPE_DEFAULTS[key]
+
+    # fill in missing data for states currently in the dataset
+    for _, state_info in state_policies.items():
+        for k, v in keys_and_defaults.items():
+            if k not in state_info.keys():
+                state_info[k] = v
+
+    # add non-present states with default values
+    non_present_states = list(
+        set(states) - set(state_policies.keys()))
+    for state in non_present_states:
+        state_policies[state] = keys_and_defaults
+
+    # sort dataset by state name
+    # code adapted from: https://www.geeksforgeeks.org/python-sort-a-dictionary/
+    state_policies = {key: val for key, val in sorted(
+        state_policies.items(), key=lambda ele: ele[0])}
+
+
+def to_json(g, i, m, w):
+    """
+    Dump state policy data to json file(s)
+
+    Inputs:
+        g (dictionary): gestational limit policy by state
+        i (dictionary): insurance coverage policy by state
+        m (dictionary): minors policy by state
+        w (dictionary): waiting periods by state
+    """
+
+    print("Writing out to json file")
+    with open("data/abortion_policy_api_gestational.json", "w") as f:
+        json.dump(g, f, indent=1)
+
+    with open("data/abortion_policy_api_insurance.json", "w") as f:
+        json.dump(i, f, indent=1)
+
+    with open("data/abortion_policy_api_minors.json", "w") as f:
+        json.dump(m, f, indent=1)
+
+    with open("data/abortion_policy_api_waiting.json", "w") as f:
+        json.dump(w, f, indent=1)
+
+
+def clean():
+    """
+    Cleans API dataset by filling in missing values.
+    """
+
+    states = get_states('data/states.txt')
+
+    # get API data
+    policy_areas = []
+    with open('data/abortion_policy_api_gestational_unclean.json', 'r') as f:
+        policy_areas.append(json.load(f))
+
+    with open('data/abortion_policy_api_insurance_unclean.json', 'r') as f:
+        policy_areas.append(json.load(f))
+
+    with open('data/abortion_policy_api_minors_unclean.json', 'r') as f:
+        policy_areas.append(json.load(f))
+
+    with open('data/abortion_policy_api_waiting_unclean.json', 'r') as f:
+        policy_areas.append(json.load(f))
+
+    # fill in missing data
+    for state_policies in policy_areas:
+        fix_missing_data(state_policies, states)
+
+    # convert dataset to json
+    g, i, m, w = policy_areas
+    to_json(g, i, m, w)
