88470cd8d6037508f05bebcbc74da5dc298100ed
Kate Habich <85372986+ehabich@users.noreply.github.com>
2023-02-08 16:18:50 -0600

    Create visualization abstract class (#8)
    Co-authored-by: Kate Habich <Kate@MacBook-Pro-5.local>
    Co-authored-by: michplunkett <michplunkett@gmail.com>

---
+++ visualizations/vis_class.py
@@ -0,0 +1,24 @@
+from collections.abc import MutableMapping
+from abc import abstractmethod
+
+
+class Visualization(MutableMapping):
+    @abstractmethod
+    def __init__(self, filename):
+        pass
+
+    @abstractmethod
+    def _import_file(self):
+        """
+        This method accesses a JSON file and returns a dictionary of data for
+        the visualization.
+        """
+        pass
+
+    @abstractmethod
+    def create(self):
+        """
+        This method takes the dictionary made in the _import_file() method and
+        creates a visualization with it.
+        """
+        pass
