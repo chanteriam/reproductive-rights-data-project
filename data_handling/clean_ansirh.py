721b6baa6285ab4830d79928f8197f81e9c8df06
Kate Habich <Kate@MacBook-Pro-5.local>
2023-02-13 17:38:22 -0600

    expand and update data munge of ASNIRH

--- data_handling/clean_ansirh.py
+++ data_handling/clean_ansirh.py
@@ -5,7 +5,8 @@ Authors: Kate Habich
 """
 
 import pandas as pd
-from .apis.abortion_policy_api import TYPE_DEFAULTS
+import re
+from apis.abortion_policy_api import TYPE_DEFAULTS
 
 # TYPE_DEFAULTS = {"str": None, "bool": False, "int": 0, "float": 0.0}
 
@@ -21,11 +22,23 @@ def clean_data():
 
     complete_ansirh = replace_missing_values(ansirh)
 
-    # TODO: lowercase everything
-    # TODO: turn 'yes' to True, 'no' to False
+    for col in complete_ansirh.columns:
+        if col != "facility name" and col != "full address" and \
+            type(complete_ansirh[col][0]) == str or \
+            complete_ansirh[col][0] == None:
 
-    # return cleaned_data
-    pass
+            for row in complete_ansirh[col]:
+                # lowercase everything
+                row.lower().strip('\"')
+
+                # turn 'yes' to True, 'no' to False
+                ### is this actually going to replace the value?###
+                if row == 'yes':
+                    row = True
+                if row == 'no':
+                    row = False
+
+    return complete_ansirh
 
 
 def replace_missing_values(data):
@@ -39,8 +52,9 @@ def replace_missing_values(data):
         for dtype, _ in TYPE_DEFAULTS.items():
             ### TODO: cant index row of this non-NA column ###
             ### TODO: replace eval() with getattr()
-            print(type(data[col][data[col].notnull()][1]))
-            if isinstance(type(data[col][data[col].notnull()][0]), eval(dtype)):
+            not_null = data[col][data[col].notnull()]
+            print(type(not_null))
+            if isinstance(type((data[col])[data[col].notnull()][0]), eval(dtype)):
                 print(f"{col}: {eval(dtype)}")
                 data[col].fillna(TYPE_DEFAULTS[dtype])
 
--- data_handling/munge_ansirh.py
+++ data_handling/munge_ansirh.py
@@ -3,10 +3,74 @@ Munge ANSIRH data into JSON files binned by state.
 """
 
 import pandas as pd
+from .clean_ansirh import clean_data
 
 
 def main_ansirh():
     """
     Create state dictionary of data from ANSIRH.
     """
+    ansirh = clean_data()
+
+    row_dicts = make_row_dict(ansirh)
+
+    # state_dict = split_by_state(ansirh)
+
+    # ### flip this to go from micro --> macro (row to state)
+    # for state in state_dict:
+    #     zip_dict = split_by_zip(state)
+
+    #     for row in zip_dict:
+    #         row_dict = make_row_dict(row)
+
+    # pass
+
+
+def split_by_state(data):
+    '''
+    Create dictionary of states matched to a list of tuples of row data
+    '''
+
+    state_dict = {}
+    for row in data.itertuples():
+        full_state = translate_code_to_state(row["state"])
+        if not state_dict[full_state]:
+            state_list = [row]
+
+        else:
+            state_list.append(row)
+
+        state_dict[full_state] = state_list
+            
+    return state_dict
+
+
+def translate_code_to_state(state_abr):
+    '''
+    Turns two-letter state code into the full state name.
+    '''
+
+    # read in state abreviation data
+    file_name = "./data/state_abbreviations.csv"
+    state = pd.read_csv(file_name)
+
+    state_name = state["state"][state["code"] == state_abr.upper()]
+
+    return state_name
+
+
+def split_by_zip():
+    '''
+    Split state dictionary by zip.
+    '''
     pass
+
+
+def make_row_dict(data):
+    '''
+    Create dictionary from column name and information of each row.
+    '''
+
+    row_dict = data.to_dict('records')
+
+    return row_dict
