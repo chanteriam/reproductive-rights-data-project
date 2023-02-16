a5ab5daa343deb1be1491797a68fa091bf165062
michplunkett <michplunkett@gmail.com>
2023-02-16 15:54:37 -0600

    Change package name

---
+++ visualization/service.py
@@ -0,0 +1,8 @@
+"""
+The service file will pull together all the visualization from their
+respective files so that they can be run in one place.
+"""
+
+
+def main():
+    print("We are gonna do some visualization")
