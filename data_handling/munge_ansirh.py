c0a5bb1c3d0dd99db01ad2188d0285489328ea4e
Kate Habich <Kate@MacBook-Pro-5.local>
2023-02-19 23:02:40 -0600

    finalizes munge_ansirh functionality without clean

--- data_handling/munge_ansirh.py
+++ data_handling/munge_ansirh.py
@@ -3,121 +3,107 @@ Munge ANSIRH data into JSON files binned by state.
 """
 
 import pandas as pd
-import csv
-from .clean_ansirh import clean_data
+
+# from .clean_ansirh import clean_ansirh
 
 
 def main_ansirh():
     """
     Create state dictionary of data from ANSIRH.
     """
-    
+
     ansirh = pd.read_csv("./data/AFD_2021_for_ArcGIS_Upload.csv")
 
     # drop empty column
-    ansirh = ansirh.drop(['Unnamed: 2'], axis=1)
+    ansirh = ansirh.drop(["Unnamed: 2"], axis=1)
 
     # make dictionaries of every row in dataset
     row_dicts = make_row_dicts(ansirh)
 
-    # TODO: sort by zipcode
-    zip_dict = split_by_zip(row_dicts)
-
     # TODO: clean data
-        # send through shays clean functions
-    clean_zip_dict = clean_data(zip_dict)
+    # send through modified shays clean functions
+    # clean_row_dicts = clean_ansirh(row_dicts)
 
+    #####
     # TODO: sort by state
+    # state_dict = split_by_state(clean_row_dicts)
+    state_dict = split_by_state(row_dicts)
 
-    state_dict = split_by_state(clean_zip_dict)
+    # TODO: sort by zipcode
+    zip_dict = split_by_zip(state_dict)
+
+    # TODO: write to JSON
+
+    return zip_dict
 
 
-    return state_dict
-
-
-def split_by_state(zips):
-    '''
-    ### NOT DONE ###
-    Create dictionary of states containing list of zip dictionaries
-    '''
+def split_by_state(rows):
+    """
+    Create dictionary of states containing list of row dictionaries
+    """
 
     state_dict = {}
-    for zip in zips:
-        for row in
-        full_state = translate_code_to_state(["state"])
-        if not state_dict[full_state]:
-            state_list = [row]
+    for row in rows:
 
+        # translate two letter state code to full name
+        full_state = str(translate_code_to_state(row["state"]))
+
+        # create states dictionary with row dicts list as value
+        if full_state not in state_dict:
+            state_dict[full_state] = [row]
         else:
-            state_list.append(row)
+            state_dict[full_state].append(row)
 
-        state_dict[full_state] = state_list
-            
     return state_dict
-
-
-    # zip_dict = {}
-    # for row in rows:
-    #     if row["zip code"] not in zip_dict:
-    #         zip_dict[row["zip code"]] = [row]
-
-    #     else:
-    #         zip_dict[row["zip code"]].append(row)
-    
-    # return zip_dict
 
 
 def translate_code_to_state(state_abr):
-    '''
+    """
     Turns two-letter state code into the full state name.
-    '''
+    """
 
     # read in state abreviation data
     file_name = "./data/state_abbreviations.csv"
     state = pd.read_csv(file_name)
-
 
     state_name = state["state"][state["code"] == state_abr.upper()]
 
     return state_name
 
 
-def split_by_zip(rows):
-    '''
+def split_by_zip(state_dict):
+    """
     Split state dictionary by zip.
 
     Inputs:
-        rows (list): list of row dictionaries
+        state_dict (dict): dictionary of states with values set to list of
+            row dicts
 
-    Returns (dict): zip dictinary containing lists of rows in that zip
-    '''
+    Returns (dict): state dict containing zip dicts containing lists of rows in
+            that zip
+    """
 
-    zip_dict = {}
-    for row in rows:
-        if row["zip code"] not in zip_dict:
-            zip_dict[row["zip code"]] = [row]
+    complete_dict = {}
+    for state, rows in state_dict.items():
+        zip_dict = {}
+        for row in rows:
+            if row["zip code"] not in zip_dict:
+                zip_dict[row["zip code"]] = [row]
+            else:
+                zip_dict[row["zip code"]].append(row)
 
-        else:
-            zip_dict[row["zip code"]].append(row)
-    
-    return zip_dict
+        # set state value to be dicitonary of zip codes
+        # containing list of row dictionaries
+        complete_dict[state] = zip_dict
+
+    return complete_dict
 
 
 def make_row_dicts(data):
-    '''
+    """
     Create dictionary from column name and information of each row.
-    '''
+    """
 
-    row_dict = data.to_dict('records')
+    row_dict = data.to_dict("records")
 
     return row_dict
-
-
-
-                    
-                    
-
-
-
-
-        
