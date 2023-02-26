9af6a1e8beef47bf4af16ddf47874f5496cd2fee
Kate Habich <85372986+ehabich@users.noreply.github.com>
2023-02-26 14:44:32 -0600

    Update util and main (#20)
    Authored-by: Kate Habich <Kate@MacBook-Pro-5.local>

---
+++ util/functions.py
@@ -0,0 +1,27 @@
+"""
+Creates functions for multiple use across files.
+
+Author(s): Kate Habich
+"""
+import pandas as pd
+
+
+def translate_code_to_state(state_abr):
+    """
+    Turns two-letter state code into the full state name.
+
+    Inputs:
+        state_abr (str): two letter state abbreviation
+
+    Returns (str):
+        Full state name.
+    """
+    # TODO: Will refactor with csv in the near future
+    # Read in state abreviation data
+    file_name = "./data/state_abbreviations.csv"
+    state = pd.read_csv(file_name)
+
+    # Convert to full state name
+    state_name = state["state"][state["code"] == state_abr.upper()].values[0]
+
+    return state_name
