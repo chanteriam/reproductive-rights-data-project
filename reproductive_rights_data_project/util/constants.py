8eb5ac11c47c511dcbc2fcf734cf8d40fd47403e
michplunkett <michplunkett@gmail.com>
2023-03-05 22:15:35 -0600

    Update constants.py

--- reproductive_rights_data_project/util/constants.py
+++ reproductive_rights_data_project/util/constants.py
@@ -11,23 +11,21 @@ STANDARD_ENCODING = "utf-8"
 # File Names
 # Author(s): Michael Plunkett
 BASE_DATA_DIR = "reproductive_rights_data_project/data/"
-FILE_NAME_STATE_NAMES = BASE_DATA_DIR + "states.txt"
-FILE_NAME_STATE_ABBREVIATIONS = BASE_DATA_DIR + "state_abbreviations.csv"
 FILE_NAME_ABORTION_POLICY_API_GESTATION = (
-    BASE_DATA_DIR + "abortion_policy_api_gestational.json"
+        BASE_DATA_DIR + "abortion_policy_api_gestational.json"
 )
 FILE_NAME_ABORTION_POLICY_API_INSURANCE = (
-    BASE_DATA_DIR + "abortion_policy_api_insurance.json"
+        BASE_DATA_DIR + "abortion_policy_api_insurance.json"
 )
 FILE_NAME_ABORTION_POLICY_API_MINORS = (
-    BASE_DATA_DIR + "abortion_policy_api_minors.json"
+        BASE_DATA_DIR + "abortion_policy_api_minors.json"
 )
 FILE_NAME_ABORTION_POLICY_API_WAITING_PERIOD = (
-    BASE_DATA_DIR + "abortion_policy_api_waiting.json"
+        BASE_DATA_DIR + "abortion_policy_api_waiting.json"
 )
 FILE_NAME_ANSIRH_BASE_DATA = BASE_DATA_DIR + "AFD_2021_for_ArcGIS_Upload.csv"
 FILE_NAME_ANSIRH_CLEAN_DATA = BASE_DATA_DIR + "clean_ansirh.json"
-
+FILE_NAME_STATE_NAMES = BASE_DATA_DIR + "states.txt"
 FILE_NAME_STATE_ABBREVIATIONS = BASE_DATA_DIR + "state_abbreviations.csv"
 
 # API Variables
